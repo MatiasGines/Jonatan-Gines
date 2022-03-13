c0d88b81cf6098d0d21b739921a3341f15bb8e91
MatiasGines <41976674+MatiasGines@users.noreply.github.com>
2022-03-13 13:13:32 -0300

    cambio img prueba carousel
    cambio img prueba carousel

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/*document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+
+  obtener el ancho de pantalla en una variable, dentro de un if, si la variable es mayor a 1050px .classList.toggle("container-xl")*/ 
+
+
+  function mostrar (){
+      var ancho= window.innerWidth;
+      return console.log(ancho);
+  }
+
+ 
+ /* @media (min-width: 1050px) { 
+    
+    main {
+      
+    }
+
+   }
+  */
